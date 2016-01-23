b6b7d493fa97eaa79d457d6e0159419da50881b3
Isaac Whitfield <iwhitfield@appcelerator.com>
2016-01-23 16:03:42 +0000

    Flip comment styles

--- test/test.c
+++ test/test.c
@@ -12,8 +12,8 @@
 
 
 int main () {
-		test(4402678656023170274, "0123456789ABCDEF", "hello", 2, 4);			// test default hash
-		test(4402678656023170274, "0123456789ABCDEFF", "hello", 2, 4); 		// extra byte ignored
-		test(14986662229302055855UL, "0123456789ABCDEF", "hello", 4, 8);	// check compression changes
+		test(4402678656023170274, "0123456789ABCDEF", "hello", 2, 4);			/* test default hash */
+		test(4402678656023170274, "0123456789ABCDEFF", "hello", 2, 4); 		/* extra byte ignored */
+		test(14986662229302055855UL, "0123456789ABCDEF", "hello", 4, 8);	/* check compression changes */
 		return 0;
 }
